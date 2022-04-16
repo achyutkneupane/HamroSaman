612cedfa207bca8ee731c03c45c1c819ef28e4b9
Achyut Neupane <to@achyut.com.np>
2022-04-16 20:02:09 +0545

    Migrations and relations are defined for comments and replies

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -23,6 +23,10 @@ class Product extends Model
     {
         return $this->hasOne(Auction::class);
     }
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
 
     public function scopeCategorySearch($query, $category)
     {
